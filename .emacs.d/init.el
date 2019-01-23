c8c6a291f3b4d74fad8861554eeb36e00db2a4e7
Matt Gauger <matt.gauger@gmail.com>
2019-01-23 16:44:32 -0600

    Add basic emacs config, use asdf package manager

---
+++ .emacs.d/init.el
@@ -0,0 +1,20 @@
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+ 	     '("melpa2" . "http://www.mirrorservice.org/sites/melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa3" . "http://www.mirrorservice.org/sites/stable.melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+;; Always try to install packages (rather than having to pass :ensure t to every package:
+(setq use-package-always-ensure t)
